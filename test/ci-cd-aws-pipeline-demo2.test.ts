81265c3eb6aaaf42faffab305535fbd740113a48
Seikida <seikida@hotmail.com>
2023-02-24 18:04:43 +0900

    up

---
+++ test/ci-cd-aws-pipeline-demo2.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CiCdAwsPipelineDemo2 from '../lib/ci-cd-aws-pipeline-demo2-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ci-cd-aws-pipeline-demo2-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CiCdAwsPipelineDemo2.CiCdAwsPipelineDemo2Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
